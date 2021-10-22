f9f83c66c089071586d9424b66a24c08e5111ca0
CarolinaLopera <carolinalopera08@gmail.com>
2021-10-21 21:46:48 -0500

    Recursive function

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -3,43 +3,80 @@
 #include "sort.h"
 
 /**
+ * recursive_loop - Entry point
+ *
+ * Return: Always void
+ */
+void recursive_loop(int *array, int *cpy, size_t size, size_t count)
+{
+	size_t i;
+	count++;
+
+	if (count == size)
+		return;
+	
+	for (i = 0; i < size; i++)
+	{
+		if (array[i] > array[i + 1])
+		{
+			cpy[i] = array[i + 1];
+			cpy[i + 1] = array[i];
+
+			array[i] = cpy[i];
+			array[i + 1] = cpy[i + 1];			
+
+			print_array(cpy, size);
+			printf("\n");
+		}
+	}
+	// printf("count: %li -- size: %li\n", count, size);
+	recursive_loop(array, cpy, size, count);
+}
+
+/**
  * bubble_sort - Entry point
  *
  * Return: Always void
  */
 void bubble_sort(int *array, size_t size)
 {
-	size_t i;
+	size_t i, count = 0;
 	int *cpy = malloc(size * sizeof(int));
 	(void)array, (void)size;
-	// printf("%ls", array);
-	// printf("%li", size);
 
 	for (i = 0; i < size; i++)
 		cpy[i] = array[i];
 
-	// printf("cpy: ");
-	// print_array(cpy, size);
-
-	for (i = 0; i < size; i++)
-	{
-		if (array[i] > array[i + 1])
-		{
-			// printf("cpy[i]: %i -- cpy[i + 1]: %i\n",  cpy[i], cpy[i + 1]);
-			cpy[i] = array[i + 1];
-			cpy[i + 1] = array[i];
-			// printf("cpy[i]: %i -- cpy[i + 1]: %i\n",  cpy[i], cpy[i + 1]);
-
-			array[i] = cpy[i];
-			array[i + 1] = cpy[i + 1];			
-
-			// print_array(array, size);
-			print_array(cpy, size);
-			printf("\n");
-
-			// for (j = 0; j < size; j++)
-			// 	array[j] = cpy[j];
-		}
-	}
-	
+	recursive_loop(array, cpy, size, count);
 }
+
+
+
+
+
+// printf("%ls", array);
+// printf("%li", size);
+
+// printf("cpy: ");
+// print_array(cpy, size);
+
+// for (i = 0; i < size; i++)
+// {
+// 	if (array[i] > array[i + 1])
+// 	{
+// 		// printf("cpy[i]: %i -- cpy[i + 1]: %i\n",  cpy[i], cpy[i + 1]);
+// 		cpy[i] = array[i + 1];
+// 		cpy[i + 1] = array[i];
+// 		// printf("cpy[i]: %i -- cpy[i + 1]: %i\n",  cpy[i], cpy[i + 1]);
+
+// 		array[i] = cpy[i];
+// 		array[i + 1] = cpy[i + 1];			
+
+// 		// print_array(array, size);
+// 		print_array(cpy, size);
+// 		printf("\n");
+
+// 		// for (j = 0; j < size; j++)
+// 		// 	array[j] = cpy[j];
+// 	}
+// }
