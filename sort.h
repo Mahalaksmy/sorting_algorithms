64f7b71719f9576f72489295496341ab58a7b628
CarolinaLopera <carolinalopera08@gmail.com>
2021-10-25 17:42:05 -0500

    Fix betty

--- sort.h
+++ sort.h
@@ -10,19 +10,17 @@
  */
 typedef struct listint_s
 {
-    const int n;
-    struct listint_s *prev;
-    struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
-void print_array(const int *array, size_t size);
+/* Prototypes*/
+
 void print_list(const listint_t *list);
-
 void bubble_sort(int *array, size_t size);
-void quick_sort(int *array, size_t size);
-void selection_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
 
-void recursive_loop(int *array, int *cpy, size_t size, size_t count);
-
-#endif
+#endif /* SORT_H  */
